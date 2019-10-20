6edee6a2f95f0230622df053efaafecccc96235b
egorbarisovi4 <egorbarisovi4@gmail.com>
2019-10-20 19:32:21 +0500

    init object positions

---
+++ lesson-7.js
@@ -0,0 +1,33 @@
+'use strict';
+
+let positions = [
+    {
+      title: 'Телепорт бытовой VZHIH-101',
+      producer: {
+        name: 'Рязанский телепортостроительный завод',
+        deferPeriod: 10,
+        lot: 3
+      },
+      price: 10000
+    },
+    {
+      title: 'Ховерборд Mattel 2016',
+      producer: {
+        name: 'Волжский Ховерборд Завод',
+        deferPeriod: 24,
+        lot: 14
+      },
+      price: 9200
+    },
+    {
+      title: 'Меч световой FORCE (синий луч)',
+      producer: {
+        name: 'Тульский оружейный комбинат',
+        deferPeriod: 5,
+        lot: 1
+      },
+      price: 57000
+    }
+  ];
+  
+  
